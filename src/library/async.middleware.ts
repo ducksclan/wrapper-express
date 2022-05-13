8dd1f3b497179a1ae334fd43c3c732fa563c2677
hitachi567 <abidai790@gmail.com>
2022-05-13 18:20:33 +0300

    async middleware was added

---
+++ src/library/async.middleware.ts
@@ -0,0 +1,23 @@
+import { Query as ParsedQs } from 'express-serve-static-core';
+import * as WrapperExpress from '.';
+
+/**
+ * Asynchronous request handler with automatic exception catching
+ * @param middleware request handler
+ * @returns middleware function
+ */
+export default function asyncMiddleware<
+    Body = any,
+    Locals extends WrapperExpress.Locals = WrapperExpress.Locals,
+    Query extends ParsedQs = ParsedQs
+>(
+    middleware: WrapperExpress.Middleware<Body, Locals, Query>
+): WrapperExpress.Middleware<Body, Locals, Query> {
+    return async (request, response, next) => {
+        try {
+            await middleware(request, response, next);
+        } catch (error) {
+            next(error);
+        }
+    };
+}
