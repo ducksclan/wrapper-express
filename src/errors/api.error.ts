fbdf2dcf449ffc8038bcf4271aeae912fb7b42cd
hitachi567 <abidai790@gmail.com>
2022-06-01 21:43:15 +0300

    doc was added to ApiError class

--- src/errors/api.error.ts
+++ src/errors/api.error.ts
@@ -1,8 +1,27 @@
 import { HttpStatus } from '../interfaces';
 
+/**
+ * Extended error class for api handling needs.
+ * @example
+ * const message = 'error message';
+ * const debugMessage = 'debug message';
+ * const stausCode = HttpStatus.BadRequest;
+ * const error = new ApiError(message, stausCode, debugMessage);
+ *
+ * console.log(error instanceof ApiError) // true
+ * console.log(error instanceof Error) // true
+ *
+ * response.status(error.status).send(error.message);
+ */
 export default class ApiError extends Error {
     name: 'ApiError';
+    /**
+     * Http status code
+     */
     status: HttpStatus;
+    /**
+     * Debug message
+     */
     debug?: string;
 
     constructor(message?: string, status?: HttpStatus, debug?: string) {
